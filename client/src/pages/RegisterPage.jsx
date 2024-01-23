96e069d409b1b8093db557e9fcf3b9d700b074ea
Shubhankar Gadad <shubhankargadad967@gmail.com>
2024-01-23 09:54:25 +0530

    Setup the server and styling part

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "../styles/Register.scss";
+import { useState } from "react";
 
 
 const RegisterPage = () => {
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    profileImage: null,
+  });
+
+  const handlePage = (e) => {
+    const { name, value, files } = e.target
+    setFormData({
+      ...formData,
+      //* this is used to concatenate the values of the previous inputs. if we remove this only the current value being written gets stored
+      [name]: value,
+      [name]: name === "profileImage" ? files[0] : value
+    });
+  }
+
   return (
     <div className="register">
       <div className="register_content">
@@ -11,20 +31,40 @@ const RegisterPage = () => {
             type="text"
             placeholder="First Name"
             name="firstname"
+            value={formData.firstname}
+            onChange={setFormData}
             required
           />
-          <input type="text" placeholder="Last Name" name="lastname" required />
-          <input type="email" placeholder="Email" name="email" required />
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="lastname"
+            value={formData.lastname}
+            onChange={setFormData}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={formData.email}
+            onChange={setFormData}
+            required
+          />
           <input
             type="password"
             placeholder="Password"
             name="password"
+            value={formData.password}
+            onChange={setFormData}
             required
           />
           <input
             type="password"
             placeholder="Confirm Password"
             name="password"
+            value={formData.confirmPassword}
+            onChange={setFormData}
             required
           />
           <input
@@ -32,6 +72,7 @@ const RegisterPage = () => {
             type="file"
             name="profileImage"
             accept="image/*"
+            onChange={setFormData}
             required
             style={{ display: "none" }}
           ></input>
@@ -39,6 +80,13 @@ const RegisterPage = () => {
             <img src="/assets/addImage.png" alt="add profile pic" />
             <p>Upload Your Photo</p>
           </label>
+          {formData.profileImage && (
+            <img
+              src={URL.createObjectURL(formData.profileImage)}
+              alt="profile pic"
+              style={{maxWidth: "80px"}}
+            />
+          )}
           <button type="submit" className="button">
             REGISTER
           </button>
